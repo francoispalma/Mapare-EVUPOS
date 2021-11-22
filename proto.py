d0e10970d898e97be120c917765ac525b69c2993
onilateur <francois-palma@etud.univ-tln.fr>
2021-11-22 05:36:44 +0100

    Adds triangle display (?)

--- proto.py
+++ proto.py
@@ -42,11 +42,83 @@ class voxel(object):
         self.width = width
 
 
-if __name__ == "__name__":
-    glutInit()
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA)
+VERTEX_SHADER = """
+ 
+#version 140
+ 
+    in vec4 position;
+    void main() {
+    gl_Position = position;
+ 
+}
+ 
+ 
+"""
+ 
+ 
+ 
+FRAGMENT_SHADER = """
+#version 140
+ 
+    void main() {
+ 
+    gl_FragColor = 
+ 
+    vec4(1.0f, 0.0f,0.0f,1.0f);
+ 
+    }
+ 
+"""
+ 
+shaderProgram = None
+quadric = None
 
+def init():
+    global quadric
+    global VERTEXT_SHADER
+    global FRAGMEN_SHADER
+    global shaderProgram
+
+    vertexshader = shaders.compileShader(VERTEX_SHADER, GL_VERTEX_SHADER)
+    fragmentshader = shaders.compileShader(FRAGMENT_SHADER, GL_FRAGMENT_SHADER)
+
+    shaderProgram = shaders.compileProgram(vertexshader, fragmentshader)
+    glClearColor(0.1, 0.1, 0.1, 1.0)
+    
+    triangles = [-0.5, -0.5, 0.0,
+                 0.5, -0.5, 0.0,
+                 0.0, 0.5, 0.0]
+    
+    triangles = np.array(triangles, dtype=np.float32)
+    
+    VBO = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)
+    glBufferData(GL_ARRAY_BUFFER, triangles.nbytes, triangles, GL_STATIC_DRAW)
+    
+    position = glGetAttribLocation(shaderProgram, 'position')
+    glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 0, None)
+    glEnableVertexAttribArray(position)
+#    quadric = gluNewQuadric()
+#    gluQuadricDrawStyle(quadric, GLU_FILL)
+    
+
+def display():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    
+    glUseProgram(shaderProgram)
+    glDrawArrays(GL_TRIANGLES, 0, 3)
+    glUseProgram(0)
+    
+    glutSwapBuffers()
+
+if __name__ == "__main__":
+    glutInit()
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA)
     glutCreateWindow('proto')
     glutReshapeWindow(512, 512)
+    
+    init()
+    
+    glutDisplayFunc(display)
 
     glutMainLoop()
