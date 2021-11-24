b578c60914d6dbededa81962e81e5e708f5daee0
El-Sashok <reaubourg.alex@gmail.com>
2021-11-24 14:20:05 +0100

    refactor

---
+++ Triangle.py
@@ -0,0 +1,63 @@
+from OpenGL.GL import *
+import numpy as np
+
+
+class Triangle2D(object):
+    def __init__(self, s1, s2, s3):
+        self.s1 = s1
+        self.s2 = s2
+        self.s3 = s3
+
+    def __repr__(self):
+        return f'triangle2D(s1:{self.s1}, s2:{self.s2}, s3:{self.s3})'
+
+
+class Triangle3D(object):
+    def __init__(self, s1, s2, s3, color=(1, 0, 0)):
+        self.s1 = s1
+        self.s2 = s2
+        self.s3 = s3
+        self.color = color
+        self.voxlist = []
+
+    def __repr__(self):
+        return f'triangle3D(s1:{self.s1}, s2:{self.s2}, s3:{self.s3}, color:{self.color}, voxlist:{self.voxlist})'
+
+    def draw(self):
+        glBegin(GL_TRIANGLES)
+
+        glColor3f(*self.color)
+        for sommet in [self.s1, self.s2, self.s3]:
+            glVertex3f(*sommet)
+
+        glEnd()
+
+    def normalize(self, maqs=None):
+        if maqs is None or maqs == 0:
+            maqs = 0
+            for coord in self.s1 + self.s2 + self.s3:
+                if abs(coord) > maqs:
+                    maqs = abs(coord)
+
+        if maqs == 0:
+            raise ValueError
+
+        self.s1 = (np.array(self.s1) / maqs).tolist()
+        self.s2 = (np.array(self.s2) / maqs).tolist()
+        self.s3 = (np.array(self.s3) / maqs).tolist()
+        for voxel in self.voxlist:
+            voxel.normalize(maqs)
+
+    def add_voxel(self, voxel):
+        self.voxlist += [voxel]
+
+    def draw_voxels(self):
+        for voxel in self.voxlist:
+            voxel.draw()
+
+    def project(self, axis):
+        if type(axis) == int:
+            return Triangle2D(self.s1[:axis] + self.s1[axis + 1:],
+                              self.s2[:axis] + self.s2[axis + 1:],
+                              self.s3[:axis] + self.s3[axis + 1:])
+        # TODO: if axis == X, Y, Z
